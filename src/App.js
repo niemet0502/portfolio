f5679886ac81f480911e2c19fdfef7644539cbd8
niemet0502 <vincentmarius8@gmail.com>
2020-12-26 13:43:27 +0100

    main :: added loader component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import { useState, useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
+import Loader from "./component/Loader/Loader"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+    
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {loading === false ? (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+         ) : (
+          <Loader />
+        )}
+    </>
   );
 }
 
