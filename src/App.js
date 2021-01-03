70fd0ccc9e8f2bc199fc176404364bb5195a1d51
niemet0502 <vincentmarius8@gmail.com>
2021-01-03 15:21:20 +0100

    main :: added detection theme user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
 import Loader from "./component/Loader/Loader"
+import styled from "styled-components"
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -14,26 +15,11 @@ function App() {
   }, [])
   return (
     <>
-    {loading === false ? (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <div className="TextCard">
+            Marius
+          </div>
         </div>
-         ) : (
-          <Loader />
-        )}
     </>
   );
 }
